1717f121847cb0dd7fc1785910af27c6461c774c
minj0i <h_hoh@naver.com>
2020-06-14 14:52:27 +0900

    Updates

---
+++ precache-manifest.a2cc935e2e309fe3fda34eeece2c8f34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75df1e367fa40880951f64f60b387557",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "c57c39d989f0056ac893",
+    "url": "/movie_app_2020/static/css/main.8e718231.chunk.css"
+  },
+  {
+    "revision": "c50adcdf695eba86cb7d",
+    "url": "/movie_app_2020/static/js/2.af139e7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.af139e7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c57c39d989f0056ac893",
+    "url": "/movie_app_2020/static/js/main.22ef9ab8.chunk.js"
+  },
+  {
+    "revision": "86e4940ccaeed0f37e85",
+    "url": "/movie_app_2020/static/js/runtime-main.83f6590a.js"
+  }
+]);
